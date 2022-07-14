4dfede587ade3af657edf34899b0c33dc78c9197
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-07-13 23:47:05 -0700

    Fix bugs
    - Line breaks weren't shown when viewing the note content.
      Fixed the issue by replacing the <p> with a disabled <textarea>.
      Also applied some styles to that <textarea>.
    - .gitignore database file.
    - Another bug was that an event listener was being added to the .alert
      element before it was added to the dom.
      Fixed the bug by first checking for the .alert existence before trying
      to add an event listener to it.

--- static/script.js
+++ static/script.js
@@ -1,5 +1,8 @@
 const closeBtn = document.querySelector(".alert .btn-close");
-closeBtn.addEventListener("click", () => {
-    const alertDiv = document.querySelector(".alert");
-    alertDiv.remove();
-});
+
+if (closeBtn) {
+    closeBtn.addEventListener("click", () => {
+        const alertDiv = document.querySelector(".alert");
+        alertDiv.remove();
+    });
+}
